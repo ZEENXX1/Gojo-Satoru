f1c5ad626d5f68950497b00f791362acd407011a
ZEENXX1 <108700007+ZEENXX1@users.noreply.github.com>
2022-07-05 21:56:31 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 //═══════════════════════════════════════════════════════//
 //
-//                              𝙶𝙾𝙹𝙾-𝚂𝙰𝚃𝙾𝚁𝚄 𝓫𝔂 𝓷𝓮𝔁𝓾𝓼𝓝𝔀
-//𝙰𝙳𝙾𝙿𝚃𝙴𝙳 𝙵𝚁𝙾𝙼  𝚂𝙲𝚁𝙸𝙿𝚃 𝙾𝙵 𝙲𝙷𝙴𝙴𝙼𝚂𝙱𝙾𝚃 𝚅2 𝙱𝚈 𝙳𝙶𝚇𝚎𝚘𝚗 
+//                              KakaRot Bot By Amine
+//KakaRot Bot help From Zenx1
 //
 //════════════════════════════//
 
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.owner = ['+212656337859']
+global.premium = ['+212656337859']
+global.ownernomer = '+212656337859'
+global.ownername = 'KAKA ROT'
+global.botname = 'KakaRot Bot'
+global.footer = '© KakaRot Inc.'
+global.ig = '???'
+global.region = 'Morocco, CasaBlanca, '
+global.sc = 'https://wa.me/+212656337859'
+global.myweb = 'https://www.instagram.com/invites/contact/?i=1q0h3scevmzmn&utm_content=3hkq3ep'
+global.packname = 'Kaka Rot'
+global.author = 'NikMok'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
